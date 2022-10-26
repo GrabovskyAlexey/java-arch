5d5a768287bfbb6118d206adb2959561fe022b1b
GrabovskyAlexey <grabovsky.alexey@gmail.com>
2022-10-26 21:29:44 +0300

    Homework 8

--- http-server/src/main/java/ru/grabovsky/config/ConfigFactory.java
+++ http-server/src/main/java/ru/grabovsky/config/ConfigFactory.java
@@ -2,9 +2,13 @@ package ru.grabovsky.config;
 
 public class ConfigFactory {
     public static Config getConfig(String[] args){
+        // Магическое число
         if(args.length == 2){
             return new ConfigFromCli(args);
         } else {
+            // Захардкожен путь к файлу, возможно стоит
+            // дать пользователю вариант указать свой файл и
+            // только если файл не указан брать файл по умолчанию
             return new ConfigFromFile("./../../../system.properties");
         }
     }
